265cd5e20684a75c27d4acec01d22f0c46a8ccf2
giancarlorhodes <giancarlo.rhodes@gmail.com>
2021-06-11 10:44:14 -0500

    prior to server-side rendering

---
+++ webpack.base.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+
+    mode: 'development',  
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                options: {
+                    presets: [
+                        'react', 'stage-2',
+                        ['env', { targets: { browsers: ['last 2 versions'] } }]
+                    ]
+                },
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
+
+};
